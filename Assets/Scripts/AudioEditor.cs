0e1fcbaf9351ec0c6ab5e5183c60046174f7d1b8
Brett Taylor <batzerk@gmail.com>
2021-05-26 21:31:58 +0200

    +Uncomitted changes

---
+++ Assets/Scripts/AudioEditor.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AudioEditor {
+    // Properties
+    const float SampleQuietThreshold = 0.17f;//TODO: Make a slider for this in the App Options.
+    //const float NormalizeMin = 0.1f;
+    const float NormalizeMax = 0.9f;
+    const int InitialSamplesToIgnore = 20000; // in case there's a bump in the beginning of the recording, just don't register it.
+    const int SamplesToIncludeBeforeFirstLoudIndex = 15000; // include just a moment before the threshold point, to make it less potentially abrupt.
+
+
+    public static AudioClip GetQuietTrimmed(AudioClip clip) {
+        float[] samples = new float[clip.samples * clip.channels];
+        clip.GetData(samples, 0);
+
+        // TRIM.
+        int firstLoudIndex = 0;
+        for (int i = Mathf.Min(samples.Length, InitialSamplesToIgnore); i < samples.Length; ++i) {
+            if (samples[i] > SampleQuietThreshold) {
+                firstLoudIndex = i;
+                break;
+            }
+        }
+        firstLoudIndex = Mathf.Max(0, firstLoudIndex - SamplesToIncludeBeforeFirstLoudIndex);
+        float[] newSamples = new float[samples.Length - firstLoudIndex];
+        for (int i = 0; i < newSamples.Length; i++) {
+            newSamples[i] = samples[i + firstLoudIndex];
+        }
+
+        Debug.Log("TRIM. samples.Length: " + samples.Length + ",   firstLoudIndex: " + firstLoudIndex);
+
+        clip = AudioClip.Create(clip.name, newSamples.Length, clip.channels, clip.frequency, false);
+        clip.SetData(newSamples, 0);
+        return clip;
+    }
+    public static AudioClip GetNormalized(AudioClip clip) {
+        float[] samples = new float[clip.samples * clip.channels];
+        clip.GetData(samples, 0);
+
+        float highestPeak = 0;
+        for (int i = 0; i < samples.Length; ++i) {
+            highestPeak = Mathf.Max(highestPeak, Mathf.Abs(samples[i]));
+        }
+        float normalizeFactor = NormalizeMax / highestPeak;
+        normalizeFactor = Mathf.Clamp(normalizeFactor, 1f, 3f); // Don't normalize softer, or get TOO loud.
+        for (int i = 0; i < samples.Length; ++i) {
+            samples[i] *= normalizeFactor;
+        }
+
+        Debug.Log("NORMALIZE. normalizeFactor: " + normalizeFactor + ",  highestPeak: " + highestPeak);
+
+        clip = AudioClip.Create(clip.name, samples.Length, clip.channels, clip.frequency, false);
+        clip.SetData(samples, 0);
+        return clip;
+    }
+
+
+    public static AudioClip GetTrailingSilenceTrimmed(AudioClip clip) {
+        float[] samples = new float[clip.samples * clip.channels];
+        clip.GetData(samples, 0);
+
+        // Determine how many new samples there are.
+        int numNewSamples = samples.Length; // default to all of 'em.
+        for (int i=samples.Length-1; i>=0; --i) {
+            if (samples[i] != 0) {
+                numNewSamples = i;
+                break;
+            }
+        }
+        Debug.Log("TRAILING SILENCE TRIMMED. original samples: " + clip.samples + ",  new samples: " + numNewSamples);
+        // Make/return the new clip!
+        clip = AudioClip.Create(clip.name, numNewSamples, clip.channels, clip.frequency, false);
+        clip.SetData(samples, 0);
+        return clip;
+    }
+
+}
---
+++ Assets/Scripts/Menus/StudySetToughies.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class StudySetToughiesTile : MonoBehaviour {
+    // Properties
+    [SerializeField] private RectTransform myRectTransform;
+    [SerializeField] private RectTransform rt_numTermsCardIcon;
+    [SerializeField] private TextMeshProUGUI t_name;
+    [SerializeField] private TextMeshProUGUI t_numTerms;
+    [SerializeField] private GameObject go_progressBar;
+    [SerializeField] private Image i_progressBarBack;
+    [SerializeField] private Image i_progressBarFillRecent;
+    [SerializeField] private Image i_progressBarFillYeses;
+    // References
+    private StudySet mySet;
+
+
+
+    // ----------------------------------------------------------------
+    //  Start
+    // ----------------------------------------------------------------
+    private void Start() {
+        mySet = GameManagers.Instance.DataManager.library.setSourdough;
+        rt_numTermsCardIcon.localEulerAngles = new Vector3(0, 0, Random.Range(-5f, 5f));
+
+        UpdateVisuals();
+    }
+
+
+
+    // ----------------------------------------------------------------
+    //  UpdateVisuals
+    // ----------------------------------------------------------------
+    public void UpdateVisuals() {
+        // Update visuals
+        t_name.text = mySet.name;
+        t_numTerms.text = mySet.NumInCurrentRound.ToString();
+        go_progressBar.SetActive(mySet.IsInProgress);
+        if (mySet.IsInProgress) {
+            float barWidth = i_progressBarBack.rectTransform.rect.width;
+            float progLocYeses = (mySet.NumTotal - (mySet.pileYesesAndNosG.Count + mySet.pileQueueG.Count)) / (float)mySet.NumTotal;
+            float progLocRecent = mySet.NumDone / (float)mySet.NumTotal;
+            float yesWidth = barWidth * progLocYeses;
+            i_progressBarFillYeses.rectTransform.sizeDelta = new Vector2(yesWidth, i_progressBarFillYeses.rectTransform.sizeDelta.y);
+            i_progressBarFillRecent.rectTransform.anchoredPosition = new Vector2(yesWidth, 0);
+            i_progressBarFillRecent.rectTransform.sizeDelta = new Vector2(barWidth * progLocRecent, i_progressBarFillRecent.rectTransform.sizeDelta.y);
+        }
+    }
+
+
+
+    // ----------------------------------------------------------------
+    //  Events
+    // ----------------------------------------------------------------
+    public void OnClickedStudyMe() {
+        GameManagers.Instance.EventManager.OpenPanel_StudyFlashcards(mySet);
+    }
+    //public void OnClick_RemakeSet() {
+    //    GameManagers.Instance.DataManager.RefillSourdoughSet();
+    //    UpdateVisuals();
+    //}
+    //public void OnClickedEditMe() {
+    //    myPanel.OnClickedEditASet(mySet);
+    //}
+}
